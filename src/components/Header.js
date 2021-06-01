da20af10331166919c5713be11093c6dc1898719
AKatryukhin <A.Katryukhin@yandex.ru>
2021-06-01 12:09:43 +0300

    feat: added Route to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,26 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import logo from '../images/header-logo.svg';
 import { Link } from 'react-router-dom';
 import { AppContext } from '../contexts/AppContext';
 
 function Header({ signOut }) {
-  const value = React.useContext(AppContext);
+const value = React.useContext(AppContext);
 
   return (
       <header className='header page__header'>
         <img className='header__logo' src={logo} alt='Логотип Место' />
         <div className='header__link-container'>
         {value.loggedIn && <span className='header__user-mail'>{value.userData.email}</span>}
-        {!value.loggedIn &&
-          <Link className='header__link' to='/signup'>{!value.loggedIn && !value.isRegist ? 'Регистрация' : 'Войти'}</Link>
-        }
-        {value.loggedIn &&
+        <Route path="/signup">
+          <Link className='header__link' to='/signup'>Войти</Link>
+        </Route>
+        <Route path="/signin">
+          <Link className='header__link' to="signup">Регистрация</Link>
+        </Route>
+        <Route path="/main">
           <Link onClick={signOut}  className='header__link' to='/signin'>Выйти</Link>
-        }
+        </Route>
           </div>
       </header>
   );
