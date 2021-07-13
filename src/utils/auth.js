f8db6b87bd594bf124dceb741267b0e13b0bb972
AKatryukhin <A.Katryukhin@yandex.ru>
2021-07-13 15:35:03 +0300

    fic auth

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -24,7 +24,6 @@ export const authorize = ({ email, password })  => {
     method: 'POST',
     credentials: 'include',
     headers: {
-      'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({ email, password })
